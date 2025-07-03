83538937a508cb3a027e1b15ed87d41b36b0ffdf
1f10240009 <s1f102400099@iniad.org>
2025-07-03 11:55:38 +0900

    修正1

--- todo/tests.py
+++ todo/tests.py
@@ -3,9 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 # Create your tests here.
+
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelCase(TestCase):
     def test_create_task1(self):
@@ -16,7 +19,9 @@ class TaskModelCase(TestCase):
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, "task1")
         self.assertFalse(task.completed)
-        self.assertEqual(task.due_at,due)
+        self.assertEqual(task.due_at, due)
+
+
 
     def test_create_task2(self):
         task = Task(title="task2")
@@ -25,9 +30,11 @@ class TaskModelCase(TestCase):
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, "task2")
         self.assertFalse(task.completed)
-        self.assertEqual(task.due_at,None)
+        self.assertEqual(task.due_at, None)
         
+
 class TaskModelTestCase(TestCase):
+
 
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -37,6 +44,8 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
     
+
+
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
@@ -45,6 +54,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertTrue(task.is_overdue(current))
     
+
     def test_is_overdue_none(self):
         due = None
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
@@ -53,24 +63,29 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
+
+
     def test_index_get(self):
         client = Client()
         response = client.get("/")
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(len(response.context["tasks"]),0)
+        self.assertEqual(len(response.context["tasks"]), 0)
+
 
     def test_index_post(self):
         client = Client()
         data = {"title": "Test Task", "due_at": "2024-06-30 23:59:59"}
-        response = client.post("/",data)
+        response = client.post("/", data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(len(response.context["tasks"]),1)
+        self.assertEqual(len(response.context["tasks"]), 1)
     
+
     def test_index_get_order_post(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
@@ -81,8 +96,9 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(response.context["tasks"][0],task2)
-        self.assertEqual(response.context["tasks"][1],task1)
+        self.assertEqual(response.context["tasks"][0], task2)
+        self.assertEqual(response.context["tasks"][1], task1)
+
 
     def test_index_get_order_due(self):
         task1 = Task(title="task1", due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -94,5 +110,5 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
-        self.assertEqual(response.context["tasks"][0],task1)
-        self.assertEqual(response.context["tasks"][1],task2)
+        self.assertEqual(response.context["tasks"][0], task1)
+        self.assertEqual(response.context["tasks"][1], task2)
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+
 
 # Create your views here.
 def index(request):
@@ -19,3 +21,15 @@ def index(request):
         "tasks" : tasks
     }
     return render(request, "todo/index.html", context)
+
+
+def detail(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+
+    context = {
+        "task" : task,
+    } 
+    return render(request, "todo/detail.html", context)
